2c5ca914d6005fd9419bd218ed8f33d0a192e580
Ayush Bulbule <ayushbulbule24@gmail.com>
2022-10-09 09:27:07 +0530

    Quotes App Created!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaQuoteLeft } from 'react-icons/fa'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [quotesData, setQuotesData] = useState([]);
+  const [quote, setQuote] = useState(null);
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    getQuote();
+  }, [])
+
+  useEffect(()=>{
+    getNewQuote()
+  },
+    [quotesData])
+
+
+  const getQuote = () => {
+    fetch('https://type.fit/api/quotes')
+      .then(response => response.json())
+      .then(data => setQuotesData(data))
+  }
+
+  const getNewQuote = () => {
+    if (quotesData.length > 0) {
+      let randomQuote = quotesData[Math.floor(Math.random() * quotesData.length)];
+      console.log("hi");
+      setQuote(randomQuote.text);
+      setAuthor(randomQuote.author);
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-slate-900 h-screen flex items-center justify-center ">
+      <div className="card-container bg-sky-500 rounded-md h-2/3 w-2/4 flex items-center justify-center">
+        <div className="card-container flex justify-between  bg-gray-100 rounded-md h-2/3 w-3/5 flex-col shadow-lg">
+          <div className="box pt-8 px-8 overflow-hidden">
+            <FaQuoteLeft className='text-start text-4xl text-gray-400' />
+            <h5 className='text-gray-800 text-center text-xl mt-6' style={{ fontFamily: '"Satisfy", cursive' }}>
+              {(quote === null) ? "Quote is Here!" : quote}
+            </h5>
+            <p className="text-sm text-gray-600 text-right mt-4" style={{ fontFamily: '"Poppins",sans-seri' }}>{(author === null) ? "Ayush Bulbule" : author}</p>
+          </div>
+          <p onClick={() => { getNewQuote(); }} className='bg-purple-700 cursor-pointer w-full text-center p-2 text-white font-medium '>New Quote</p>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+    <h5>GPA </h5>
+    <h6>Hello Om Hi!!</h6>
+    </div>
+  )
+}
+
+export default Home
